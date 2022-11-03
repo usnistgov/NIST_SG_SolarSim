66e4d147500c3530c21e7ffdadae973f80b58717
Allen Goldstein <allen.goldstein@nist.gov>
2022-11-03 13:44:59 -0400

    working on versio number

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setuptools_kwargs = {
 extensions = []
 
 setup(name=DISTNAME,
+      version='0.2.0',
       packages=PACKAGES,
       package_data=PACKAGE_DATA,
       install_requires=INSTALL_REQUIRES,
